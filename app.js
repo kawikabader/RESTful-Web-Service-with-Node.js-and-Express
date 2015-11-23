c3599d88bec2ae18ce221f47468199c31518761f
Kawika Bader <kawika.bader@pfcb.com>
2015-11-22 18:44:20 -0700

    [0.6] Implementing HTTP Get

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+  .get(function (request, response) {
+    var responseJson = { hello: 'This is my API.' };
+    
+    // Sending json response
+    response.json(responseJson);
+  });
+
+app.use('/api', bookRouter);
+
 app.get('/', function (req, res) {
   res.send('Welcome to my API!');
 });
