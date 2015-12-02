21ffbb6338d9e06b9081f68a86d6f01b95d311db
Kawika Bader <kawika.bader@pfcb.com>
2015-12-02 13:24:43 -0700

    [0.8] Added middleware.

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -1,57 +1,57 @@
 var express = require('express');
 
 var routes = function (Book) {
-  var bookRouter = express.Router();
+var bookRouter = express.Router();
 
-  bookRouter.route('/')
-    .post(function (request, response) {
-      var book = new Book(request.body);
+bookRouter.route('/')
+  .post(function (request, response) {
+    var book = new Book(request.body);
 
-      book.save();
-      response.status(201).send(book);
+    book.save();
+    response.status(201).send(book);
 
-    })
-    .get(function (request, response) {
+  })
+  .get(function (request, response) {
 
-      var query = {};
+    var query = {};
 
-      if (request.query.genre) {
-        query.genre = request.query.genre;
-      }
-      Book.find(query, function (error, books) {
-        if (error)
-          response.status(500).send(error);
-        else
-          response.json(books);
-      });
+    if (request.query.genre) {
+      query.genre = request.query.genre;
+    }
+    Book.find(query, function (error, books) {
+      if (error)
+        response.status(500).send(error);
+      else
+        response.json(books);
     });
+  });
 
+bookRouter.use('/:bookId', function (request, response, next) {
+  Book.findById(request.params.bookId, function (error, book) {
+    if (error)
+      response.status(500).send(error);
+    else if (book) {
+      request.book = book;
+      next();
+    } else {
+      response.status(404).send('No book found...');
+    }
+  });
+});
 
-  bookRouter.route('/:bookId')
-    .get(function (request, response) {
-
-      Book.findById(request.params.bookId, function (error, book) {
-        if (error)
-          response.status(500).send(error);
-        else
-          response.json(book);
-      });
-    })
-    .put(function (request, response) {
-      Book.findById(request.params.bookId, function (error, book) {
-        if (error)
-          response.status(500).send(error);
-        else {
-          book.title = request.body.title;
-          book.author = request.body.author;
-          book.genre = request.body.genre;
-          book.read = request.body.read;
-          book.save();
-          response.json(book);
-        }
-      })
-    });
-  return bookRouter;
+bookRouter.route('/:bookId')
+  .get(function (request, response) {
+    response.json(request.book);
+  })
+  .put(function (request, response) {
+      request.book.title = request.body.title;
+      request.book.author = request.body.author;
+      request.book.genre = request.body.genre;
+      request.book.read = request.body.read;
+      request.book.save();
+      response.json(request.book);
+});
+return bookRouter;
 };
 
 module.exports = routes;
