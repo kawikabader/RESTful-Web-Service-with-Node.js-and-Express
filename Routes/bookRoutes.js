8fe6b289657f0ba5026fcb943997ae6b034c705d
Kawika Bader <kawika.bader@pfcb.com>
2015-12-01 20:04:23 -0700

    [0.7] Saving data, code cleanup, injecting our model.

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var routes = function(Book) {
+  var bookRouter = express.Router();
+
+  bookRouter.route('/')
+    .post(function(request, response){
+    var book = new Book(request.body);
+
+    book.save();
+    response.status(201).send(book);
+
+  })
+    .get(function (request, response) {
+
+    var query = {};
+
+    if (request.query.genre)
+    {
+      query.genre = request.query.genre;
+    }
+    Book.find(query, function (error, books) {
+      if (error)
+        response.status(500).send(error);
+      else
+        response.json(books);
+    });
+  });
+
+
+  bookRouter.route('/:bookId')
+    .get(function (request, response) {
+
+    Book.findById(request.params.bookId, function (error, book) {
+      if (error)
+        response.status(500).send(error);
+      else
+        response.json(book);
+    });
+  });
+  return bookRouter;
+};
+
+module.exports = routes;
--- app.js
+++ app.js
@@ -14,45 +14,10 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-bookRouter.route('/Books')
-  .post(function(request, response){
-    var book = new Book(request.body);
-
-    console.log(book);
-    response.send(book);
-
-  })
-  .get(function (request, response) {
-
-    var query = {};
-
-    if (request.query.genre)
-    {
-      query.genre = request.query.genre;
-    }
-    Book.find(query, function (error, books) {
-      if (error)
-        response.status(500).send(error);
-      else
-        response.json(books);
-    });
-  });
-
-
-bookRouter.route('/Books/:bookId')
-.get(function (request, response) {
-
-    Book.findById(request.params.bookId, function (error, book) {
-      if (error)
-        response.status(500).send(error);
-      else
-        response.json(book);
-    });
-  });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
+app.use('/api/authors', authorRouter);
 
 app.get('/', function (req, res) {
   res.send('Welcome to my API!');
