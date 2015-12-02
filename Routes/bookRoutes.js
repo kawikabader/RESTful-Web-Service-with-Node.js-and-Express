331c24565115bcec28e607b6f079afbf96fd0c13
Kawika Bader <kawika.bader@pfcb.com>
2015-12-02 13:10:17 -0700

    [0.8] Implementing put verb.

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -1,43 +1,56 @@
 var express = require('express');
 
-var routes = function(Book) {
+var routes = function (Book) {
   var bookRouter = express.Router();
 
   bookRouter.route('/')
-    .post(function(request, response){
-    var book = new Book(request.body);
+    .post(function (request, response) {
+      var book = new Book(request.body);
 
-    book.save();
-    response.status(201).send(book);
+      book.save();
+      response.status(201).send(book);
 
-  })
+    })
     .get(function (request, response) {
 
-    var query = {};
+      var query = {};
 
-    if (request.query.genre)
-    {
-      query.genre = request.query.genre;
-    }
-    Book.find(query, function (error, books) {
-      if (error)
-        response.status(500).send(error);
-      else
-        response.json(books);
+      if (request.query.genre) {
+        query.genre = request.query.genre;
+      }
+      Book.find(query, function (error, books) {
+        if (error)
+          response.status(500).send(error);
+        else
+          response.json(books);
+      });
     });
-  });
 
 
   bookRouter.route('/:bookId')
     .get(function (request, response) {
 
-    Book.findById(request.params.bookId, function (error, book) {
-      if (error)
-        response.status(500).send(error);
-      else
-        response.json(book);
+      Book.findById(request.params.bookId, function (error, book) {
+        if (error)
+          response.status(500).send(error);
+        else
+          response.json(book);
+      });
+    })
+    .put(function (request, response) {
+      Book.findById(request.params.bookId, function (error, book) {
+        if (error)
+          response.status(500).send(error);
+        else {
+          book.title = request.body.title;
+          book.author = request.body.author;
+          book.genre = request.body.genre;
+          book.read = request.body.read;
+          book.save();
+          response.json(book);
+        }
+      })
     });
-  });
   return bookRouter;
 };
 
